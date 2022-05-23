5b6894e0061f596a60ff717150c4d941b0aee18f
Carlos Eduardo Oliveira Filho <caduoliveiraf@gmail.com>
2022-05-23 18:54:38 -0300

    first commit

---
+++ src/cadu/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package cadu.dio;
+
+import cadu.dio.model.Cachorro;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Cachorro dog = new Cachorro();
+
+        System.out.println(dog);
+
+
+    }
+}
